51b8e28a4f6d6002937ac941b5c3cc68d974b7a7
Tom Holmes <tholmes59@gmail.com>
2019-03-25 19:21:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,11 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    erb :create_puppy
+    erb :index
   end
   
   get '/new' do
     
+    erb :create_puppy
   end 
   
   get '/puppy' do
